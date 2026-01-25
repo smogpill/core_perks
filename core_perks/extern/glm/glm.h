758020c2e3eaccff5de7d77fd75f6765c74ccfe0
smogpill <smogpill@gmail.com>
2026-01-25 21:53:03 +0100

    Cleaning

--- core_perks/extern/glm/glm.h
+++ core_perks/extern/glm/glm.h
@@ -2,3 +2,22 @@
 // SPDX-FileCopyrightText: 2025 Jounayd ID SALAH
 // SPDX-License-Identifier: MIT
 #pragma once
+
+#ifdef CP_GLM
+
+template<typename T>
+glm::tvec3<T> safe_normalize(const glm::tvec3<T>& v, T epsilon = T(1e-6))
+{
+    T lengthSquared = glm::dot(v, v);
+    if (lengthSquared < epsilon * epsilon)
+        return glm::tvec3<T>(0);
+    return v * glm::inversesqrt(lengthSquared);
+}
+
+template<typename T, glm::qualifier Q>
+glm::mat<4, 4, T, Q> to_left_up_forward(const glm::mat<4, 4, T, Q>& mat)
+{
+    return glm::mat4(mat[0], mat[1], -mat[2], mat[3]);
+}
+
+#endif
--- core_perks/math/geometry/aabox.h
+++ core_perks/math/geometry/aabox.h
@@ -13,7 +13,11 @@ namespace cp
 		CP_FORCE_INLINE AABox() = default;
 		CP_FORCE_INLINE AABox(const Vec3<T>& min, const Vec3<T>& max) : min_(min), max_(max) {}
 
-		CP_FORCE_INLINE bool valid() const { return (min_ <= max_).all(); }
+		CP_FORCE_INLINE void clear() { *this = AABox(); }
+		CP_FORCE_INLINE bool valid() const { return greater_or_equal(max_, min_).all(); }
+		CP_FORCE_INLINE Vec3<T> get_size() const { return max_ - min_; }
+
+		static const AABox& invalid() { static const AABox invalid; return invalid; }
 
 		Vec3<T> min_ = 1e15f;
 		Vec3<T> max_ = -1e15f;
